29396701436fb5d73c3766d6374308e6d09dde93
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-11-16 17:36:05 -0600

    Complete exercise 4a

--- src/components/ShoeGrid/ShoeGrid.jsx
+++ src/components/ShoeGrid/ShoeGrid.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+`;
+
+const ShoeWrapper = styled.div`
+  width: 275px;
+  flex-grow: 1;
+`;
 
 export default ShoeGrid;
